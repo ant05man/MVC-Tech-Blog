ae54b8fcd3a783d171d7f02cdaea9a893ad4dc0e
Anthony Littlejohn <ant05man@yahoo.com>
2023-10-01 10:27:58 -0400

    created files for api folder

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,139 @@
+const router = require('express').Router();
+//const sequelize = require('../../config/connection');
+const { Post, User, Comment } = require('../../models');
+ const withAuth = require('../../utils/auth');
+
+// get all users
+router.get('/', (req, res) => {
+  
+  Post.findAll({
+    attributes: [
+      'id',
+      // 'url',
+      'title',
+      'body',
+      'created_at',
+    ],
+    order: [['created_at', 'DESC']],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(postData => res.json(postData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get('/:id', (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      // 'url',
+      'title',
+      'body',
+      'created_at',
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(postData => {
+      if (!postData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(postData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post('/', withAuth, (req, res) => {
+  Post.create({
+    title: req.body.title,
+    body: req.body.body,
+    // url: req.body.url,
+    user_id: req.session.user_id
+  })
+    .then(postData => res.json(postData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.put('/:id', withAuth, (req, res) => {
+  Post.update(
+    {
+      title: req.body.title,
+      body: req.body.body
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+    .then(postData => {
+      if (!postData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(postData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.delete('/:id', withAuth, (req, res) => {
+  console.log('id', req.params.id);
+  Post.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(postData => {
+      if (!postData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(postData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
