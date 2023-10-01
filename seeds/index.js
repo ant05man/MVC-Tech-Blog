ac4b8dc07ac5b41e0cc407aa6cd9226804656c56
Anthony Littlejohn <ant05man@yahoo.com>
2023-10-01 09:16:16 -0400

    added code for index.js inside seeds

---
+++ seeds/index.js
@@ -0,0 +1,17 @@
+const sequelize = require('../config/connection');
+
+// links to other seeds files
+const seedUsers = require('./user-seeds');
+const seedPosts = require('./post-seeds');
+const seedComments = require('./comment-seeds');
+
+
+const seedAll = async() => {
+    await sequelize.sync({ force: true });
+    await seedUsers();
+    await seedPosts();
+    await seedComments();
+    process.exit(0);
+};
+
+seedAll();
