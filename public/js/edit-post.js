3647b44e3944e382dc09370f3afbf85b39206447
Anthony Littlejohn <ant05man@yahoo.com>
2023-10-01 10:15:54 -0400

    added content to files inside public folder

---
+++ public/js/edit-post.js
@@ -0,0 +1,27 @@
+async function editFormHandler(event) {
+    event.preventDefault();
+  
+    const title = document.querySelector('input[name="post-title"]').value.trim();
+    const body = document.querySelector('textarea[name="post-body"]').value.trim();
+    const id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
+    const response = await fetch(`/api/posts/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        title,
+        body
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/dashboard/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
+  document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
