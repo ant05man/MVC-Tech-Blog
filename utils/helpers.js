cfbed84174d135a26f9cc42d321b8cc3aaa56f9a
Anthony Littlejohn <ant05man@yahoo.com>
2023-10-01 15:57:45 -0400

    created files for views folder

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,20 +1,10 @@
 module.exports = {
-    format_date: date => {
-        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
-      },
-      format_url: url => {
-        return url
-            .replace('http://', "")
-            .replace('https://', "")
-            .replace('www.', "")
-            .split('/')[0]
-            .split('?')[0];
-    },
-      format_plural: (word, amount) => {
-        if (amount !== 1) {
-          return `${word}s`;
-        }
-    
-        return word;
-      }
-    };
+  format_date: (date) => {
+    // Format date as MM/DD/YYYY
+    return date.toLocaleDateString();
+  },
+  format_amount: (amount) => {
+    // format large numbers with commas
+    return parseInt(amount).toLocaleString();
+  },
+};
