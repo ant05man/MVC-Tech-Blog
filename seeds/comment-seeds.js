727dc6ae1d687fd516d38ee28c956e05ea6f110a
Anthony Littlejohn <ant05man@yahoo.com>
2023-10-01 09:14:07 -0400

    added other comments for comment-seeds.js

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -6,4 +6,18 @@ const commentData = [{
         user_id: 1,
         post_id: 1
     },
+    {
+        comment_text: "Second comment",
+        user_id: 2,
+        post_id: 2
+    },
+    {
+        comment_text: "Another comment",
+        user_id: 3,
+        post_id: 3
+    }
 ];
+
+const seedComments = () => Comment.bulkCreate(commentData);
+
+module.exports = seedComments;
